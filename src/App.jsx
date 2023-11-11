e3394a11103f08a39d566b1f22e7bd81d3f813b5
Mahijith <menon.mahijith@gmail.com>
2023-11-11 22:03:33 +0530

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -223,7 +223,18 @@ function App() {
               onClick={() => updateMessage(currentGroup.id)}
               className="send-btn"
             >
-              {'>'}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="29"
+                viewBox="0 0 35 29"
+                fill="none"
+              >
+                <path
+                  d="M0 29V18.125L14.5 14.5L0 10.875V0L34.4375 14.5L0 29Z"
+                  fill="#ABABAB"
+                />
+              </svg>
             </button>
           </div>
         </section>
